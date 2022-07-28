805759b815db7f7ccc8975ea5065a1c5975a4ab2
araromi <abdulhameedararomi@gmail.com>
2022-07-28 06:54:20 +0100

    feat: Add custom ser authentication refactor: name both the user app and template file 'accounts' and make use of the CSS file in static

--- qrCode_Project/urls.py
+++ qrCode_Project/urls.py
@@ -1,7 +1,11 @@
+from argparse import Namespace
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='components/landingPage.html'), name='home'),
+    path('accounts/', include(('accounts.urls', 'accounts'), namespace='accounts')),
     path('', include('qrCode_App.urls')),
 ]
