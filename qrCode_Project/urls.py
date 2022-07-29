0267aa948afd008135f18f4fe5406c730139a3e5
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2022-07-29 08:24:02 +0100

    designed the errorpage, also installed neccessary package of hosting

--- qrCode_Project/urls.py
+++ qrCode_Project/urls.py
@@ -1,5 +1,5 @@
-from argparse import Namespace
 from django.contrib import admin
+from django.conf.urls import handler400, handler403,handler404,handler500
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
@@ -9,3 +9,9 @@ urlpatterns = [
     path('accounts/', include(('accounts.urls', 'accounts'), namespace='accounts')),
     path('', include('qrCode_App.urls')),
 ]
+
+# views for custom error page
+handler404 = 'qrCode_App.views.custom_page_not_found_view'
+handler500 = 'qrCode_App.views.custom_error_view'
+handler403 = 'qrCode_App.views.custom_permission_denied_view'
+handler400 = 'qrCode_App.views.custom_bad_request_view'
