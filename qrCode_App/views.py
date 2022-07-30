86d0dac273f8f950e7a612856cf033adf14d5405
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2022-07-30 20:45:57 +0100

    update

--- qrCode_App/views.py
+++ qrCode_App/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 # Create your views here.
 
 class HomePageView(TemplateView):
-    template_name = 'base.html'
+    template_name = 'components/landingPage.html'
     
 # custom views for error pages
 def custom_page_not_found_view(request, exception):
