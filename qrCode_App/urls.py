a5153b0c109ab7a2c334f8c5d2e1b85582a1cd9a
Zainab Abdulkareem <zainab.ifedayolag@gmail.com>
2022-08-01 09:21:38 +0100

    Update branch

--- qrCode_App/urls.py
+++ qrCode_App/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path
-from .views import HomePageView
+from .views import (HomePageView, ContactUsView)
 urlpatterns = [
     path('', HomePageView.as_view(), name='Index'),
+    path('/contactus', ContactUsView.as_view(), name='contact-us')
 ]
